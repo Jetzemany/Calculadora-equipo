05d8c284a4e95582d4ab428e2fce2cce8d949691
ericktilin <20243ds101@utez.edu.mx>
2025-09-04 12:21:22 -0600

    Add files via upload

---
+++ Calculadora/src/main/java/utez/multiplicacion.java
@@ -0,0 +1,31 @@
+import java.util.scanner;
+public class Multiplicacion{
+    private float numero1;
+    private float numero2;
+    public Multiplicacion(float numero1, numero2)[
+
+            this.numero1 = numero1;
+            this.numero2 = numero2;
+    ]
+
+    public float getResultado(){
+            return numero1 * numero2;
+    }
+    public void setResultado(Float numero1, numero2){
+            this.numero1 = numero1;
+            this.numero2 = numero2;
+
+    }
+    public static void  main(String[]args){
+        Scanner numero= new  Scanner(system.in);
+       
+
+        System.out.println("Ingresa numero 1");
+        float numero1 = numero.nextFloat;
+        numero.nextLine;
+
+        System.out.println("ingresa numero 2");
+        float numero2 = numero.nextFloat;
+        numero.nextLine;
+    } 
+} 
